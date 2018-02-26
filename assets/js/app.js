27ffda183fb2cc4fc6a7221d4d7a5f6a84073ce3
conois <conii.echeverria@gmail.com>
2018-02-26 10:58:28 -0300

    mas recusos y page.js funcionando. Funciones listas para agregar contenido

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+/* page('ruta', callback) */
+page('/#', index); 
+page('/phones', phones); 
+page('/camera', camera); 
+page('/headphones', headphones); 
+page('/computers', computers);
+page({ dispatch: false })
+
+
+
+function index(){
+	$("#fromPage").
+	$("#fromPage").append
+}
+
+
+function phones() {
+	//Here go content for phones pages 
+	document.querySelector("p").textContent = 'viewing phones' 
+}
+
+function camera(){
+	//Here go content for phones pages 
+	document.querySelector("p").textContent = 'viewing camera' 
+}
+
+function headphones(){
+	//Here go content for phones pages 
+	document.querySelector("p").textContent = 'viewing headphones' 
+}
+
+function computers(){
+	//Here go content for phones pages 
+	document.querySelector("p").textContent = 'viewing computers' 
+}
